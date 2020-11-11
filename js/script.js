518503a1c37a2ac9f749b7e8de8e2987c98dc5b1
Juliane Pires <mpiresjuliane@outlook.com>
2020-11-11 14:35:59 +0000

    Criação de variáveis e modificação de pseudo elementos css.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
+var nome = document.getElementById("nome").value;
+var email = document.getElementById("email").value;
+var telefone = document.getElementById("telefone").value;
+
 function validaFormulario () {
 
-	if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != ""){
+	if( nome != "" && email != "" && telefone != ""){
 		alert("Prontinho! Você receberá as novidades por email.")
 	}else{
 		alert("Por favor, preencha os campos nome, email e telefone corretamente!")
